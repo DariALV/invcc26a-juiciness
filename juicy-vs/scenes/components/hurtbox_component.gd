d74acc8e67365e4e64f2be5bf6fbd753fbec63ff
DariALV <darialvjs@gmail.com>
2026-04-07 08:38:55 -0600

    feature(component): Add Hurtbox functionality

--- juicy-vs/scenes/components/hurtbox_component.gd
+++ juicy-vs/scenes/components/hurtbox_component.gd
@@ -1,2 +1,12 @@
 extends Area2D
 class_name HurtboxComponent
+
+@export var health : HealthComponent
+
+
+
+func _on_area_entered(area : Area2D):
+	# todo: Chequear que el area sea un HitboxComponent
+	if health:
+		# todo: Aplicar el daño del HitboxComponent 
+		health.apply_damage(10)
