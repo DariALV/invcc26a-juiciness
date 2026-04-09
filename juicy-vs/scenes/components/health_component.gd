d639ad070a520f5bd1b9b1a85b20faf846845b4f
Dariel Jesus Alvarez Gonzalez <c20413@ECCI105U407109.ecci.ucr.ac.cr>
2026-04-09 10:16:37 -0600

    fix(dead_signal): died signal is emitted

--- juicy-vs/scenes/components/health_component.gd
+++ juicy-vs/scenes/components/health_component.gd
@@ -8,10 +8,16 @@ signal health_changed(new_health : float)
 @export var max_health : float = 100
 @export var min_health : float = 0
 
+var isDead : bool = false
+
 func apply_heal(amount : float):
-	current_health = min(max_health, current_health + amount)
-	health_changed.emit(current_health)
+	if !isDead:
+		current_health = min(max_health, current_health + amount)
+		health_changed.emit(current_health)
 
 func apply_damage(amount : float):
 	current_health = max(min_health, current_health - amount)
 	health_changed.emit(current_health)
+	if current_health == 0:
+		isDead = true
+		died.emit()
