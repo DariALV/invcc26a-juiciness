ee4c46b192486d0cf502b3e9eb155366ef52c6a9
DariALV <darialvjs@gmail.com>
2026-04-13 14:25:26 -0600

    feature(manager): Add Enemy Spawner

---
+++ juicy-vs/scenes/managers/enemy_spawner/enemy_spawner.gd
@@ -0,0 +1,32 @@
+extends Node
+
+@export var enemy_scene : PackedScene
+@export var spawn_cooldown : float = 1
+@export var spawn_distance : float = 50
+
+@onready var timer : Timer = $Timer
+
+func _ready():
+	timer.wait_time = spawn_cooldown
+	timer.timeout.connect(spawn_slime)
+
+func spawn_slime():
+	var enemy : Slime = enemy_scene.instantiate() as Slime
+	enemy.global_position = get_spawn_position()
+	get_parent().add_child(enemy)
+
+func get_spawn_position():
+	var player : Player = get_tree().get_first_node_in_group("player") as Player
+	var player_pos : Vector2 = player.global_position
+	var angle = randf() * 2 * PI
+	var spawn_direction : Vector2 = polar_to_cartesian(angle, spawn_distance)
+	return player_pos + spawn_direction
+
+#TODO: Aislar a clase global
+func polar_to_cartesian(angle : float, length : float) -> Vector2:
+	# sin(angle) = y/h => y = h * sin(angle)
+	var y = length * sin(angle)
+	# cos(angle) = x/h => x = h * cos(angle)
+	var x = length * cos(angle)
+	return Vector2(x, y)
+	
