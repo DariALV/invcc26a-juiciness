b1dfcc8c4d19ddd4e14d072eed5cb4ebafe3b2fe
DariALV <darialvjs@gmail.com>
2026-04-12 22:44:28 -0600

    refactor(component): Refactored MovementComponent
    It now handles movement itself, instead of only calculating velocity

--- juicy-vs/scenes/components/movement_component.gd
+++ juicy-vs/scenes/components/movement_component.gd
@@ -4,8 +4,19 @@ class_name MovementComponent
 @export var speed : float = 1
 @export var max_speed : float = 10
 
+var parent : CharacterBody2D = null
+
+func _ready():
+	assert(get_parent() is CharacterBody2D, 
+	"MovementComponent debe ser hijo de un CharacterBody2D")
+	parent = get_parent()
+
 func calculate_velocity(direction : Vector2) -> Vector2:
 	return direction.normalized() * speed
 
 func increase_speed(amount : float):
 	speed = min(max_speed, speed + amount);
+	
+func move(direction : Vector2):
+	parent.velocity = direction.normalized() * speed
+	parent.move_and_slide()
